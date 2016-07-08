d7da0e93e65f0ba4ce1bc08439fb6d18d4a40a09
frg100 <frg100@gmail.com>
2016-07-07 22:47:34 -0400

    added support for blocks
    added support for processing files as 256 bit blocks

--- block_ca/block_ca.py
+++ block_ca/block_ca.py
@@ -9,11 +9,14 @@ config_no_to_cells = {0: '0000', 1: '1000', 2: '0100', 3: '1100', 4: '0010', 5: 
 ###### IMPORTING MODULES ########
 import numpy as np
 from random import randint
+import random
 from time import sleep
 import math
 import time
 import filecmp
 import cProfile
+from collections import OrderedDict
+import sys as Sys
 ###### IMPORTING MODULES ########
 
 
@@ -25,7 +28,7 @@ def rule_list_reversal(forward_rule_list):
         reverse_rule_list[x] = forward_rule_list.index(x) #reverses the rule_list for use in the reverse evolution
         
     return reverse_rule_list
-        
+
 def get_current_config(one, two, three, four):
     """Take in the 4 values as input and decide which of the 16 configurations (0-15) it is"""
     string = str(one) + str(two) + str(three) + str(four)
@@ -45,12 +48,13 @@ def change_config(current_config,rule_list):
 	for x in new_config_string:
 		values_list.append(int(x))
 	return values_list
-	
 
 def margolus1(rule_list,dish):
-    """Does the first part of the evolution, can be customized with the appropiate rule_list for the forward and reverse evollutions"""
+    """Does the first part of the evolution, can be customized with the appropiate rule_list for the forward and reverse evolutions"""
+    
     height = len(dish)
     width = len(dish[0])
+    
     for x in range(0,height-2,2):
 		for y in (0,width-2,2): #Starts the 2X2 box at 0,0 and then moves onto the next 2X2 box
 			square = dish[x:x+2,y:y+2] #Creates a pointer to the 2X2 part of the dish
@@ -63,9 +67,11 @@ def margolus1(rule_list,dish):
 			square[1,1] = values[3]#Changes the dish to what values it should be based on the rule_list and the current config
 
 def margolus2(rule_list,dish):
-    """Does the second part of the evolution, can be customized with the appropiate rule_list for the forward and reverse evollutions"""
+    """Does the second part of the evolution, can be customized with the appropiate rule_list for the forward and reverse evolutions"""
+    
     height = len(dish)
     width = len(dish[0])
+    
     for x in range(1,height-2,2):
 		for y in range(1,width-2,2): #Starts the 2X2 box at 1,1 (the second pass has a different neighborhood) and then moves onto the next 2X2 box
 			square = dish[x:x+2,y:y+2]
@@ -75,7 +81,7 @@ def margolus2(rule_list,dish):
 			square[0,1] = values[1]
 			square[1,0] = values[2]
 			square[1,1] = values[3]
-			
+
 def forward_evolution(generations, rule_list, dish):
     """Performs the forward evolution of the block cellular automata"""
     
@@ -84,7 +90,7 @@ def forward_evolution(generations, rule_list, dish):
         margolus2(rule_list,dish)
         
     return dish
-    
+
 def reverse_evolution(generations, rule_list, dish):
     """Performs the reverse evolution of the block cellular automata"""
     
@@ -97,7 +103,32 @@ def reverse_evolution(generations, rule_list, dish):
         
     return dish
     
-			
+def forward_block_evolution(generations, rule_list, dish_dict):
+    """Performs the forward evolution of the block cellular automata on a block_dict"""
+    
+    dishes_completed = 0
+
+    for key in dish_dict:
+        dish = dish_dict[key]
+        dish = forward_evolution(generations, rule_list, dish)
+        dishes_completed += 1
+        printProgress(dishes_completed, len(dish_dict), prefix = 'Progress:', suffix = 'Complete', barLength = 50)
+
+    return dish_dict
+    
+def reverse_block_evolution(generations, rule_list, dish_dict):
+    """Performs the reverse evolution of the block cellular automata on a block_dict"""
+    
+    dishes_completed = 0
+
+    for key in dish_dict:
+        dish = dish_dict[key]
+        dish = reverse_evolution(generations, rule_list, dish)
+        dishes_completed += 1
+        printProgress(dishes_completed, len(dish_dict), prefix = 'Progress:', suffix = 'Complete', barLength = 50)
+
+    return dish_dict
+
 def calculate_entropy_of_dish(dish):
     """Calculates the entropy of the dish"""
     bit_string = ""
@@ -121,7 +152,68 @@ def calculate_entropy_of_dish(dish):
         if freq > 0:
             ent = ent + freq * math.log(freq, 2)
     return -ent
+
+def calculate_hamming_distance(original_dish,current_dish):
+    height = len(current_dish)
+    width = len(current_dish[0])
+    hamming_distance = 0
+    total = 0
     
+    for x in range(height):
+        for y in range(width):
+            if original_dish[x][y] == current_dish[x][y]:
+                hamming_distance += 1
+                total += 1
+            else:
+                total += 1
+    return float(hamming_distance)/float(total)
+
+def get_middle_factors(number):
+    def factors(n):
+        """Function to calculate all the factors of a given number n"""
+        return sorted( reduce(list.__add__, ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)) )
+        
+    factors_list = []
+    factors_list =  factors(number)
+    #print "Factors: ", factors_list
+    middle_factors = [ factors_list[(len(factors_list)/2)-1] , factors_list[(len(factors_list)/2)] ]
+    return middle_factors
+
+def open_file_blocks(file_name):
+    """Function to open a file and create 256 bit blocks of of all the bits of the file (using random padding at the end)"""
+    
+    # read the whole file into a byte array
+    '''file_name = raw_input("What file would you like to open?: ")'''
+    f = open(file_name, "rb")
+    print file_name, "opened"
+    
+    byteArr = map(ord, f.read()) #gets byte number from whatever data the file is in
+    byteArr = map(lambda x : '{0:08b}'.format(x), byteArr) #takes the byte number and turns it into binary
+    f.close()
+    print "Filesize: ", len(byteArr)*8
+    bit_sequence = ""
+    for row in byteArr:
+        for x in row:
+            bit_sequence += str(x) #adds the numbers into a bit sequence
+    string_list = [bit_sequence[i:i+256] for i in range(0, len(bit_sequence), 256)]
+    pad_length = 256 - len(string_list[-1])
+    
+    for x in range(pad_length-8):
+        string_list[-1] += random.choice(["0","1"])
+    string_list[-1] += '{0:08b}'.format(pad_length) #append the number of pad bits
+    
+
+    dish_dict = OrderedDict()
+    for i in range(len(string_list)):
+        dish_name = "dish"+str(i)
+        dish_dict[dish_name] = np.zeros((16,16), dtype=np.int) #initializes a dish of all zeros
+        counter = 0
+        for x in range(16):
+            for y in range(16): #iterates through the whole dish
+                dish_dict[dish_name][x,y] = int(string_list[i][counter])
+                counter += 1
+                
+    return dish_dict
     
 def open_file(file_name):
     """Function to open a file and create a dish of all the bits of the file"""
@@ -153,10 +245,10 @@ def open_file(file_name):
             dish[x,y] = int(bit_sequence[counter]) #goes in order through the whole bit sequence and turns that string into an int to insert to the dish
             counter += 1
     return dish
-            
+
 def write_dish_to_file(file_name,dish):
     """Writes the dish back into a file with name = file_name """
-    print "\nWriting file %r" %file_name
+    
     bit_sequence = ""
     for row in dish:
         for cell in row:
@@ -168,5 +260,53 @@ def write_dish_to_file(file_name,dish):
     file = open(file_name,"w") #opens the file
     for i in byte_list:
         file.write(i) #writes the unicode back to file
-    print "\nFile written successfully"
+    print "File written successfully"
+
+def printProgress (iteration, total, prefix = '', suffix = '', decimals = 2, barLength = 100):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : number of decimals in percent complete (Int) 
+        barLength   - Optional  : character length of bar (Int) 
+        #http://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
+    """
+    filledLength    = int(round(barLength * iteration / float(total)))
+    percents        = round(100.00 * (iteration / float(total)), decimals)
+    bar             = '#' * filledLength + '-' * (barLength - filledLength)
+    Sys.stdout.write('%s [%s] %s%s %s\r' % (prefix, bar, percents, '%', suffix)),
+    Sys.stdout.flush()
+    if iteration == total:
+        print("\n")
+
+def blocks_to_dish(block_dict,remove_padding = False):
+    
+    bit_sequence = ""
+    for key in block_dict:
+        dish = block_dict[key]
+        for row in dish:
+            for cell in row:
+                bit_sequence += str(cell) #creates a string of all the bits
+
+    if remove_padding == True and len(bit_sequence)%256 != 0:
+        print "Removing Padding"
+        pad_length = bit_sequence[-8:]
+        pad_length = int(pad_length,2)
+        bit_sequence = bit_sequence[:-pad_length]
+
+
+    dish_size = get_middle_factors(len(bit_sequence)) #gets the middle two factors (to make the dish as square-like as possible) of a file and uses those as width and height of the file
+    height = dish_size[0]
+    width = dish_size[1] #makes the dish dimensions equal to the middle factors of the length of the dile
+    dish = np.zeros((height,width), dtype=np.int) #initializes a dish of all zeros
+    counter = 0
+    for x in range(len(dish)):
+        for y in range(len(dish[0])): #iterates through the whole dish
+            dish[x,y] = int(bit_sequence[counter]) #goes in order through the whole bit sequence and turns that string into an int to insert to the dish
+            counter += 1
+    return dish
 ########## FUNCTIONS ############
+
